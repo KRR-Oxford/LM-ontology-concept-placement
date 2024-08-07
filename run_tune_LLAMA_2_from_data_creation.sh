a107ecaecff0024102e4096527b9f5fadaa4c99a
Hang Dong <h.dong2@exeter.ac.uk>
2024-08-07 16:40:31 +0100

    Update run_tune_LLAMA_2_from_data_creation.sh

--- run_tune_LLAMA_2_from_data_creation.sh
+++ run_tune_LLAMA_2_from_data_creation.sh
@@ -1,13 +1,15 @@
 # an example of running Llama-2 fine-tuning and prompting.
 
 # first, generate prompts and instructions
-eval_set=valid-NIL,test-NIL
-./Edge-Bi-enc+prompt-generation.sh Disease true 10 5 16 > results_log_Disease_edge_biencoder_top10_bs16_final.txt 
-# setting eval_set=train in the script above for instructions, setting eval_set=valid-NIL,test-NIL in the script above for prompting only
+./Edge-Bi-enc+prompt-generation.sh Disease true 10 5 16 # > results_log_Disease_edge_biencoder_top10_bs16_final.txt 
+# need to run the above twice if instruction tuning:
+#   1. first, by setting eval_set=train in the script above for instruction prompts (for instruction tuning), 
+#   2. second, by setting eval_set=valid-NIL,test-NIL in the script above for the evaluation prompting only (for evaluation).
+# or by setting eval_set=train,valid-NIL,test-NIL to generate both instruction prompts and evaluation prompts.
 
 # go to the prompting folder
 cd blink/prompting/
-source activate onto38 # the conda environment for LLMs
+source activate ontollm38 # the conda environment for LLMs
 # change the top-k setting, model and prompt format settings before running 
 python Llama_2_finetuning.py
 python Llama_2_prompting.py
